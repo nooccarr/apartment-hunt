0a365ccf886744a6550e09f7789ec127755525cf
dylanallgood <dylanblakeallgood@gmail.com>
2020-12-20 23:12:39 -0500

    added a resolve field to prevent us having to type '.jsx' when importing
    Also removed the mode option for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
 const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: "bundle.js"
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        include: [
-          path.resolve(__dirname, "src")
-        ],
-        loader: "babel-loader",
-        options: {
-          presets: ['@babel/preset-react']
-        }
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  }
-}
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+};
