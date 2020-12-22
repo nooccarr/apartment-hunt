287a7ec07fd494e0f3b2f9d846519b19487ca4bd
NateLeeP <nlpruitt@gmail.com>
2020-12-21 23:43:46 -0500

    Render file upload component instead of app component

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App.jsx'
 import css from '../dist/styles/styles.css'
+import FileUpload from './components/FileUpload.jsx';
+import FileUploadOverlay from './components/FileUploadOverlay.jsx'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<FileUploadOverlay />, document.getElementById('app'));
