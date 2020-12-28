93a6c7ba60e810e8b99312146e04ad9816ead08a
NateLeeP <nlpruitt@gmail.com>
2020-12-28 14:40:58 -0500

    Required axios in server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
+const axios = require('axios');
 
 // Serve static assets from 'dist' folder
 app.use(express.static(path.join(__dirname, '../dist')));
