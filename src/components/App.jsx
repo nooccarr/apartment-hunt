cf10a9add9a9b6748d52e86a1897dfd75eb01baa
dylanallgood <dylanblakeallgood@gmail.com>
2020-12-28 11:39:18 -0500

    added inital routing to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import PrivateRoute from './Authentication/Auth/PrivateRoute';
+import { AuthContext } from './Authentication/Auth/AuthContext';
+import { HomeLogin, Admin, UserProfile } from './pages/index';
 
-const App = () => (
-  <div>
-    I love <span className='pink'>pink!</span>
-    I love my dog teddy!
-  </div>
-)
+const App = () => {
+  return (
+    <AuthContext.Provider value={true}>
+      <Router>
+        <div>
+          <Route exact path='/' component={HomeLogin} />
+          <PrivateRoute path='/user' component={UserProfile} />
+          <PrivateRoute path='/admin' component={Admin} />
+        </div>
+      </Router>
+    </AuthContext.Provider>
+  );
+};
 
 export default App;
