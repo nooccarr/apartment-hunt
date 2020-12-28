583b99640e3da7128070cfa9f140104c0f397a44
dylanallgood <dylanblakeallgood@gmail.com>
2020-12-28 11:43:57 -0500

    Pages folder to organize  individual components

---
+++ src/components/pages/HomeLogin.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LandingPage from '../Authentication/Login/Main';
+import './styles/homelogin.css';
+
+const HomeLogin = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+};
+
+export default HomeLogin;
