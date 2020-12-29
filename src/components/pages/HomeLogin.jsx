012346d219e7b6e62d007f343696fb0c16f124f8
Christopher <Christopherliang7@gmail.com>
2020-12-28 23:00:25 -0500

    Created Places Auto Complete Search Bar

--- src/components/pages/HomeLogin.jsx
+++ src/components/pages/HomeLogin.jsx
@@ -1,29 +1,14 @@
 import React from 'react';
-// import LandingPage from '../Authentication/Login/Main';
+import AuthLogin from '../Authentication/Login/Main';
+import HomePage from './HomePage.jsx'
 import './styles/homelogin.css';
-import searchicon from './styles/images/search-icon.png'
 
 const HomeLogin = () => {
-
   return (
-    <div>
-      {/* <LandingPage /> */}
-      <div className='container'>
-        <h1 className='Sign-In'>Log In</h1>
-      </div>
-      <div>
-        <h1 className='Logo'>Apartment Hunt</h1>
-        <p className='Slogan'>Smarter apartment search. Quality apartments you'll want to rent.</p>
-      </div>
-      <div>
-        <form className='search-container'>
-          <input className='search-bar' type='text' name='searchtext' placeholder='Enter an address, neighborhood, city, or ZIP code'/>
-          <img className='search-icon' src={searchicon} alt='search-icon'/>
-        </form>
-      </div>
-
-      <i className="searchInputIcon fa fa-search"></i>
-    </div>
+    <>
+      {/* <AuthLogin /> */}
+      <HomePage />
+    </>
   );
 };
 
