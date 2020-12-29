8c016490fc1bff1e216e1306a8e12d55064a8a54
urbanseabear <calvin.rudy@gmail.com>
2020-12-28 19:50:18 -0500

    added sample data

---
+++ database/jsonTransformer.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+
+let agents = ['John Smith', 'Penelope Jane', 'Shotaro Tanaka', 'Bill Thompson', 'Celeste Kimbrooke'];
+let raw = fs.readFileSync('/home/calvin/hackreactor/ApartmentHunt/database/173142_1.txt');
+let wholeStr = raw.toString();
+let validJSON = "[" + wholeStr.replace(/\n/g, ",") + "]";
+let allApts = JSON.parse(validJSON);
+let aptLength = allApts.length;
+fs.writeFileSync('aptClean.txt', '[');
+for (var i = 0; i < aptLength; i++) {
+    let apt = {};
+    apt.address = allApts[i].address;
+    apt.state = 'NY';
+    apt.zipCode = allApts[i].postalCode;
+    apt.city = allApts[i].city;
+    apt.country = allApts[i].country;
+    apt.description = allApts[i].descriptions[0].value;
+    apt.sqft = allApts[i].floorSizeValue;
+    apt.latitude = allApts[i].latitude;
+    apt.longitude = allApts[i].longitude;
+    if (allApts[i].listingName !== undefined) {
+        apt.listingName = allApts[i].listingName;
+      } else {
+        apt.listingName = apt.address;
+      }
+    
+    apt.neighborhoods = allApts[i].neighborhoods;
+    if (allApts[i].prices !== undefined) {
+        let price = allApts[i].prices[0].amountMin;
+        while(price > 10000) {
+           price = price / 10;
+        }
+        apt.price = price;
+      } else {
+        apt.price = "1234";
+      }
+    apt.pics = allApts[i].imageURLs;
+    petsOk = Math.random();
+    if (petsOk < .2) {
+        apt.pets = {'dogs': false, 'cats': false};
+    } else if (petsOk >= .2 && petsOk < .7) {
+        apt.pets = {'dogs': false, 'cats': true};
+    } else {
+        apt.pets = {'dogs': true, 'cats': true};
+    }
+    apt.beds = allApts[i].numBedrooms ? allApts[i].numBedrooms:(Math.floor(Math.random() * 4)) + 1;
+    apt.baths = allApts[i].numBathrooms ? allApts[i].numBedrooms:(Math.floor(Math.random() * 1)) + 1;
+    if (allApts[i].brokers !== undefined) {
+      apt.agent = allApts[i].brokers[0].agent;
+    } else {
+      let randAgent = Math.floor(Math.random() * 5);
+      apt.agent = agents[randAgent];
+    }
+
+    let aptStr = JSON.stringify(apt);
+    if (i !== aptLength - 1) {
+        aptStr = aptStr + ',';
+    }
+    fs.appendFileSync('aptClean.txt', aptStr);
+}
+fs.appendFileSync('aptClean.txt', ']');
+console.log('success meow');
