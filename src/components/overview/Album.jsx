ca914c9788d5473284b1292f3d4b6c1dbf7010ca
Noah Heath <noahadamheath@gmail.com>
2020-12-29 12:34:53 -0500

    Gallery is janky, going to refactor

--- src/components/overview/Album.jsx
+++ src/components/overview/Album.jsx
@@ -7,8 +7,22 @@ class Album extends React.Component {
         super(props);
         this.state = {
             selection: 'Photo Album',
-            options: ['Photo Album','Virtual Tour','Video Tour']
+            options: ['Photo Album','Virtual Tour','Video Tour'],
+            images: [],
+            liftGallery: '',
+            galleryTop: 0,
+            galleryLift: 0,
+            current: 0
         }
+    }
+
+    componentDidMount() {
+        var imgs=["https://www.trulia.com/pictures/thumbs_5/zillowstatic/fp/c642f5487c2888ba52ce47414851dd34-full.jpg","https://www.trulia.com/pictures/thumbs_5/zillowstatic/fp/c306e58c2da48890b1a16f4bd1c90909-full.jpg","https://www.trulia.com/pictures/thumbs_5/zillowstatic/fp/dbd316134027445b6c9a4ab16be79011-full.jpg"];
+        imgs = imgs.concat(imgs);
+        imgs = imgs.concat(imgs);
+        imgs = imgs.concat(imgs);
+        var lift = imgs.length > 10 ? 60 : 0;
+        this.setState({images: imgs, galleryTop: lift, galleryLift: lift});
     }
 
     display (option) {
@@ -27,9 +41,50 @@ class Album extends React.Component {
         src="https://my.matterport.com/show/?m=d25yC39miPY"></iframe>
     }
 
+    shift (way, e) {
+        var current = this.state.current;
+        if (way === null) {
+            console.log('shifting to ', e.target.id);
+            current = Number(e.target.id);
+        } else {
+            console.log('shifting', way ? 'right' : 'left');
+            if (current === 0 && !way) {
+                current = this.state.images.length - 1;
+            } else if (current === this.state.images.length - 1 && way) {
+                current = 0;
+            } else {
+                current += way ? 1 : -1;
+            }
+        }
+        console.log(current);
+        this.setState({current: current});
+        }
+
     carousel () {
+        var j = -1;
         return <div className="carousel">
-            carousel condiminium
+            <img className="main-image" src={this.state.images[this.state.current]}/>
+            <div className="arrow right" onClick={() => this.shift(true)}/>
+            <div className="arrow left" onClick={() => this.shift(false)}/>
+            <div className="gallery-container"
+                style={{top: 390 - this.state.galleryTop,
+                        height: 60 + this.state.galleryTop}}
+                onMouseEnter={this.state.galleryLift === this.state.galleryTop ? 
+                    () => this.setState({liftGallery: ' lift'}) : null}
+                onMouseLeave={this.state.galleryLift === 0 ?
+                    () => this.setState({liftGallery: ' drop'}): null}
+                onAnimationEnd={() => this.setState({
+                    galleryLift: this.state.liftGallery === ' lift' ? 0 : this.state.galleryTop,
+                    liftGallery: ''})}>
+                <div className={`gallery-carousel${this.state.liftGallery}`} style={this.state.images.length < 11 ? 
+                    {left: 300 - 30 * this.state.images.length} : {top: this.state.galleryLift, left: 0, width: 600}}>
+                    {_.map(this.state.images, (src) => {
+                        j++;
+                        return <img key={j} id={j} className="gallery-photo"
+                            src={src} onClick={(e) => this.shift(null, e)}/>
+                    })}
+                </div>
+            </div>
         </div>
     }
 
