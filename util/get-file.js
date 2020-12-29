3e2a35215af0ec0fbb60a30c30cdfcd2bf503933
NateLeeP <nlpruitt@gmail.com>
2020-12-29 14:54:18 -0500

    Utility functions for File Upload

---
+++ util/get-file.js
@@ -0,0 +1,32 @@
+const S3 = require('aws-sdk/clients/s3');
+
+
+
+const ID = 'AKIAI3L7HBNGPU62UHFA';
+const SECRET = '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';
+const BUCKET_NAME = 'nate-pruitt-test-bucket-0001';
+
+const s3 = new S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET
+});
+
+const getFile = (fileName, callback) => {
+  //const fileContent = fs.readFileSync(fileName);
+  const params = {
+    Bucket: 'nate-pruitt-test-bucket-0001',
+    Key: fileName,
+  };
+
+  s3.getObject(params, (err, data) => {
+    if (err) {
+      console.log("Error in download! Error: ", err);
+    }
+      console.log("File downloaded successfully!", data);
+      callback({'data': data.Body});
+  })
+};
+
+
+
+export default getFile;
---
+++ util/upload-file.js
@@ -0,0 +1,35 @@
+const S3 = require('aws-sdk/clients/s3');
+
+
+
+const ID = 'AKIAI3L7HBNGPU62UHFA';
+const SECRET = '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';
+const BUCKET_NAME = 'nate-pruitt-test-bucket-0001';
+
+const s3 = new S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET
+});
+
+const uploadFile = (fileName) => {
+  //const fileContent = fs.readFileSync(fileName);
+
+
+  const params = {
+    Bucket: 'nate-pruitt-test-bucket-0001',
+    Key: fileName.name,
+    Body: fileName, // Can this be binary files? Does it have to be a buffer? S
+    ContentType: 'application/pdf'
+  };
+
+  s3.upload(params, (err, data) => {
+    if (err) {
+      console.log("Error in upload! Error: ", err);
+    }
+    console.log("File uploaded successfully!", data)
+  })
+};
+
+
+
+export default uploadFile;
