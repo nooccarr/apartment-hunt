da4a446e382b4796830351e596efa48143161225
Christopher <Christopherliang7@gmail.com>
2020-12-30 00:23:31 -0500

    Implemented Geolocation Button onto Search Bar

--- src/components/pages/HomePage/Search.jsx
+++ src/components/pages/HomePage/Search.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PlacesAutoComplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 import regeneratorRuntime from "regenerator-runtime";
 import searchicon from '../styles/images/search-icon.png'
+import pinkmarker from '../styles/images/pink-marker.png'
 
 const HomePageSearch = () => {
   const [address, setAddress] = React.useState('');
@@ -19,8 +20,6 @@ const HomePageSearch = () => {
 
   return (
     <>
-      {console.log(address)}
-      {console.log(coordinates)}
       <div>
         <PlacesAutoComplete 
           value={address}
@@ -30,21 +29,22 @@ const HomePageSearch = () => {
           {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
             <div>
               <div className='search-container'>
-                <input className='search-bar' 
+                <img className='pink-marker' src={pinkmarker} alt='pink-marker'/>
+                <input className='search-bar'
                   {...getInputProps({placeholder: 'Enter an address, neighborhood, city, or ZIP code' })} 
-                />
+                  />
                 <img className='search-icon' src={searchicon} alt='search-icon'/>
               </div>
               <div className='search-list'>
                 {loading && <div className='search-suggestions'>Loading...</div>}
+
                 {suggestions.map(suggestion => {
                   return (
-                    <div 
-                      {...getSuggestionItemProps(suggestion)}
-                      className='search-suggestions'
-                    >
-                      {suggestion.description}
-                    </div>)
+                    <>
+                      <div {...getSuggestionItemProps(suggestion)} className='search-suggestions' >
+                        {suggestion.description}
+                      </div>
+                    </>)
                 })}
               </div>
             </div>
