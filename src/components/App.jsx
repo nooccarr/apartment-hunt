e06a60367d4eb53c1d4462fa24b42f1e1bdd249c
Noah Heath <noahadamheath@gmail.com>
2020-12-30 09:49:42 -0500

    Stuff

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import PrivateRoute from './Authentication/Auth/PrivateRoute';
 import { AuthContext } from './Authentication/Auth/AuthContext';
 import { HomeLogin, Admin, UserProfile } from './pages/index';
+import Overview from './overview/Overview.jsx';
 
 const App = () => {
   return (
