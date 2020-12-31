f05a6772af3fa50245f7c5bb92a6106bf5e32122
Christopher <Christopherliang7@gmail.com>
2020-12-31 01:19:50 -0500

    Created Light and Dark Modes for Google Maps

---
+++ src/components/SearchResults/GoogleMap/MapStyles/darkmap.js
@@ -0,0 +1,251 @@
+export default [
+  {
+      "featureType": "all",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#242f3e"
+          }
+      ]
+  },
+  {
+      "featureType": "all",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#ffccff"
+          }
+      ]
+  },
+  {
+      "featureType": "all",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+          {
+              "color": "#242f3e"
+          }
+      ]
+  },
+  {
+      "featureType": "administrative",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#242f3e"
+          }
+      ]
+  },
+  {
+      "featureType": "administrative.locality",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#ffccff"
+          },
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "administrative.land_parcel",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "landscape.man_made",
+      "elementType": "geometry.stroke",
+      "stylers": [
+          {
+              "color": "#808000"
+          },
+          {
+              "saturation": 100
+          }
+      ]
+  },
+  {
+      "featureType": "landscape.man_made",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "poi",
+      "elementType": "all",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "poi",
+      "elementType": "labels.text",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#ffccff"
+          },
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {// checkpoint
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#263c3f"
+          }
+      ]
+  },
+  {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#ffccff"
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#323a48"
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "geometry.stroke",
+      "stylers": [
+          {
+              "color": "#212a37"
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#ffccff"
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "labels.icon",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#ffccff"
+          }
+      ]
+  },
+  {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+          {
+              "color": "#1f2835"
+          }
+      ]
+  },
+  {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#ffccff"
+          }
+      ]
+  },
+  {
+      "featureType": "transit",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#2f3948"
+          },
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "transit.station",
+      "elementType": "all",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "transit.station",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#d59563"
+          },
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#17263c"
+          }
+      ]
+  },
+  {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#ffccff"
+          }
+      ]
+  },
+  {
+      "featureType": "water",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+          {
+              "color": "#17263c"
+          }
+      ]
+  }
+]
--- src/components/SearchResults/GoogleMap/index.js
+++ src/components/SearchResults/GoogleMap/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import google_api_key from '../config/google_api_key';
 import { Loader } from '@googlemaps/js-api-loader';
+import lightMap from './MapStyles/lightmap.js';
+import darkMap from './MapStyles/darkmap.js';
 
 const GoogleMap = () => {
+  // const [light, setLighting] = useState(true)
   let map;
 
   const loader = new Loader({
@@ -14,6 +17,7 @@ const GoogleMap = () => {
     map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: 40.730610, lng: -73.935242 },
       zoom: 10,
+      options: { styles: darkMap }
     });
   })
 
