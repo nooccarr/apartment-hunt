661ceb29ea8088ec0884faefaf1280e4b4c93b11
urbanseabear <calvin.rudy@gmail.com>
2020-12-31 12:02:20 -0500

    added listing post route

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -15,19 +15,58 @@ const signup = (req, res) => {
 
 const search = (req, res) => {
 console.log(req.params);
-let long = -73.92826824472331;
-let lat = 40.70237509797474;
-Apts.find().where('position').near({ center: [long, lat], maxDistance:  0.00004765343822311093, spherical: true })
+let long = -73.94443814752641;
+let lat = 40.69396233779667;
+// if (req.params.city !== undefined) {}
+// Apts.find({'state': 'NY'})
+//.then((apts) => {})
+Apts.find().where('position').near({ center: [long, lat], maxDistance:  0.00008938082823178741, spherical: true })
   .then((apts) => {
-    res.status(200).json(apts);
+    let filteredApts = [];
+    for (let i = 0; i < apts.length; i++){
+      if (apts[i].beds > 2) {
+        filteredApts.push(apts[i]);
+      }
+    }
+    res.status(200).json(filteredApts);
   })
   .catch((err) => {
     res.sendStatus(500);
   })
-}
+};
+
+const listing = (req, res) => {
+
+  let aptObj = {};
+  aptObj.address = req.body.address;
+  aptObj.listingName = req.body.address;
+  aptObj.city = "New York";
+  aptObj.state = "NY";
+  aptObj.zipCode = req.body.zipCode;
+  aptObj.country = "US";
+  aptObj.description = req.body.description;
+  aptObj.pets = {dogs: req.body.dogs, cats: req.body.cats};
+  aptObj.agent = req.body.agent;
+  aptObj.sqft = req.body.sqft;
+  aptObj.beds = req.body.beds;
+  aptObj.baths = req.body.baths;
+  aptObj.price = req.body.price;
+  aptObj.neighborhoods = req.body.neighborhoods;
+  aptObj.position = {type: "Point", coordinates: [req.body.longitude, req.body.latitude]}
+  aptObj.pics = req.body.pics;
+  Apts.create(aptObj)
+  .then(() => {
+    res.sendStatus(200);
+    console.log('meow');
+  })
+  .catch((err) => {
+    res.sendStatus(500);
+  })
+};
 
 module.exports = {
   login,
   signup,
-  search
+  search,
+  listing
 };
