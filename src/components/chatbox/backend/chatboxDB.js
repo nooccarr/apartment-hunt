60d7c3de6d5112f44e7237fa8f626c25363b534d
Kai Gong <kaigong@Kais-MacBook-Pro.local>
2020-12-31 19:18:04 -0500

    modified database schema and functions to accommodate the front-end needs

--- src/components/chatbox/backend/chatboxDB.js
+++ src/components/chatbox/backend/chatboxDB.js
@@ -17,30 +17,69 @@ db.once('open', () => {
 });
 
 const chatMessageSchema = new mongoose.Schema({
-  senderId: String,
-  messageBody: String,
+  chatId: { type: String, unique: true },
+  address: String,
+  userName: String,
+  agentName: String,
+  userId: String,
   agentId: String,
-  createdAt: String,
+  messages: [
+    {
+      message: String,
+      sender: String,
+      createdAt: String,
+    },
+  ],
 });
 
 const ChatMessage = mongoose.model('ChatMessage', chatMessageSchema);
 
-// save messages that sent from user
+// save or create messages that sent from user
 const saveMsg = (message) => {
-  const params = {
-    senderId: message.senderId,
-    messageBody: message.body,
-    agentId: message.agentId,
-    createdAt: new Date(),
-  };
-  return ChatMessage.create(params);
+  ChatMessage.find({ chatId: message.chatId }, function (err, data) {
+    if (data) {
+      return ChatMessage.findAndUpdate(message.chatId, {
+        $push: {
+          messages: {
+            message: message.body,
+            sender: message.sender,
+            createdAt: JSON.stringify(new Date()),
+          },
+        },
+      });
+    } else {
+      const params = {
+        chatId: message.chatId,
+        address: message.address,
+        userName: message.userName,
+        agentName: message.agentName,
+        userId: message.userId,
+        agentId: message.agentId,
+        message: [
+          {
+            message: message.body,
+            sender: message.sender,
+            createdAt: JSON.stringify(new Date()),
+          },
+        ],
+      };
+      return ChatMessage.create(params);
+    }
+  });
 };
 
-// fetch message history
-const fetchMsg = (query) => {
-  return ChatMessage.find({ senderId: query.senderId, agentId: query.agentId })
-    .sort('-createdAt')
-    .exec();
+// fetch message history by client/agent name
+const fetchChatsByUser = (query) => {
+  return ChatMessage.find({ userId: query.userId }).exec();
+};
+
+const fetchChatsByAgent = (query) => {
+  return ChatMessage.find({ agentId: query.agentId }).exec();
+};
+
+// fetch message by roomname/id...
+const fetchMsgById = (query) => {
+  return ChatMessage.findById(query.chatId, 'messages').exec();
 };
 
 module.exports = ChatMessage;
