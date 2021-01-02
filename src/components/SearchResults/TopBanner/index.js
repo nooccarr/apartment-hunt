457001f0ed33a5f136f905b768e7e3e872f68121
Aileen Swan <ailswanster@gmail.com>
2021-01-02 11:11:19 -0500

    implement navigationbar on the searchresult page

--- src/components/SearchResults/TopBanner/index.js
+++ src/components/SearchResults/TopBanner/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import Navigation from '../../overview/navigation.jsx';
 
 const TopBanner = ({ searchValue, setSearchValue }) => {
   return (
-    <div className='topBanner'>
-      <div className='topBannerLeft'>
-        <div className='companyName'>Apartment Hunt</div>
-        <SearchBar searchValue={ searchValue } setSearchValue={ setSearchValue } />
+    <div>
+      <Navigation searchValue={ searchValue } setSearchValue={ setSearchValue }/>
+    
+      <div className='topBanner'>
+
+        {/* <div className='topBannerLeft'>
+          <div className='companyName'>Apartment Hunt</div>
+          <SearchBar searchValue={ searchValue } setSearchValue={ setSearchValue } />
+        </div> */}
       </div>
     </div>
   );
