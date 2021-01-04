050ef511022d268515e4051de71a7554436ff9c1
NateLeeP <nlpruitt@gmail.com>
2021-01-03 19:11:49 -0500

    Code cleanup for components

--- src/components/FilePreview.jsx
+++ src/components/FilePreview.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-
-const FilePreview = ({file}) => {
+import { faFileAlt, faTrashAlt } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+const FilePreview = ({file, deleteFile}) => {
 
   return (
-  <div>
-    <div>
-      Filename: {file.name}
+  <div >
+  <div className='file-container' >
+    <div style={{'color':'#FF6EC7', 'fontSize':'large'}} >
+      <FontAwesomeIcon icon={faFileAlt} />
     </div>
+
     <div>
-      File size: {file.size}
+      <a href={URL.createObjectURL(file)} target="_blank" rel="noopener noreferrer" style={{'fontSize':'large'}}>{file.name}</a>
     </div>
-    <div>
-      <a href={URL.createObjectURL(file)} target="_blank" rel="noopener noreferrer">Preview your upload!</a>
+    <div style={{'marginLeft': 'auto', 'fontSize':'medium', 'alignSelf':'center'}}>
+      ({Number(file.size) / 1000} kb)
     </div>
+    <div onClick={(e) => {deleteFile(file.name)}} style={{'color':'#FF6EC7', 'fontSize':'medium', 'alignSelf':'center'}}>
+      <FontAwesomeIcon icon={faTrashAlt} />
+    </div>
+  </div>
+  <hr style={{'width':'600px', 'borderColor': '#FF6EC7'}}/>
   </div>
   )
 }
--- src/components/FileUploadOverlay.jsx
+++ src/components/FileUploadOverlay.jsx
@@ -1,25 +1,45 @@
 import React, {useState} from 'react';
 import FileUpload from './FileUpload.jsx';
+import Modal from '@material-ui/core/Modal';
 
 const FileUploadOverlay = () => {
   // conditional that turn it on and off??
 
-  const [hidden, setHidden] = useState(true)
+  const [open, setOpen] = useState(false)
 
   const updateOverlay = () => {
 
-    setHidden(hidden === true ? false : true);
+    setOpen(open === true ? false : true);
+  }
+
+  function getModalStyle() {
+    const top = 25;
+    const left = 25;
+
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      position: 'relative',
+      border:'none',
+      'max-width': '600px',
+      'border-radius':'10px'
+    };
   }
 
 
   return (
     <div>
-      <div onClick={() => {updateOverlay()}}>
+      <div onClick={() => {updateOverlay()}} >
         Click me! Beneath the milky twilight
       </div>
-      <div className={hidden === true ? "overlay hidden" : "overlay"}>
-        <FileUpload />
-      </div>
+      <Modal open={open} onClose={updateOverlay}>
+        <div style={getModalStyle()}>
+          <FileUpload updateOveray={updateOverlay}/>
+        </div>
+
+      </Modal>
+
+
     </div>
   )
 }
