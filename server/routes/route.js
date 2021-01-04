40e05b2e7ff67a4808c0c808f8e3294b16461f15
NateLeeP <nlpruitt@gmail.com>
2021-01-04 13:16:10 -0500

    Import document upload routes to main router

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const controller = require('../controllers/controller');
+const { downloadRoute, uploadRoute } = require('./fileUploadRoutes.js');
+const multer = require('multer');
+const upload = multer();
 
 const router = express.Router();
 
@@ -7,6 +10,10 @@ router.post('/login', controller.login);
 router.post('/signup', controller.signup);
 router.get('/search', controller.search);
 router.post('/listing', controller.listing);
+
+// File Upload Routes
+router.get('/download', downloadRoute);
+router.post('/upload', upload.any(), uploadRoute);
 
 module.exports = {
   router,
