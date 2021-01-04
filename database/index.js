7a443a4e3dbc372a032628b93c0d7d93606a32ca
urbanseabear <calvin.rudy@gmail.com>
2021-01-04 12:47:06 -0500

    fixed auth for db

--- database/index.js
+++ database/index.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-const db = mongoose.connect('mongodb://blueocean:kfCgkWe@3.133.116.139/apartmentHunt', {promiseLibrary: global.Promise});
+const db = mongoose.connect('mongodb://blueocean:kfCgkWe@3.133.116.139:27017/apartmentHunt?authSource=admin', {promiseLibrary: global.Promise});
 
 module.exports = db;
