6b557ca74c52fe878b3dd3c3a7c708c66a1390ab
NateLeeP <nlpruitt@gmail.com>
2021-01-03 19:11:12 -0500

    Add backend routes for uploading and downloading PDFs

--- server/app.js
+++ server/app.js
@@ -3,9 +3,44 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 const axios = require('axios');
+const multer = require('multer');
+const upload = multer();
+const {uploadFile, getFile, decryptMessage} = require('../util/upload-file.js');
 
 // Serve static assets from 'dist' folder
 app.use(express.static(path.join(__dirname, '../dist')));
+
+app.post('/upload', upload.any(), (req, res) => {
+  // How to pass files to endpoint?
+  console.log(req.files);
+  var promises = [];
+  req.files.forEach((file) => {
+    promises.push(uploadFile(file));
+  })
+  Promise.all(promises)
+  .then((result) => {
+    //console.log(result);
+    res.sendStatus(200);
+  })
+  .catch((err) => {
+    console.log("Error in post route! Errror: ", err);
+    res.sendStatus(500);
+  })
+})
+
+app.get('/download', (req, res) => {
+  getFile("Encrypted PDF File.pdf")
+  .then((data) => {
+    console.log('Encrypted message return: ', data['Body'])
+    //console.log('Decrypt Message Return: ', decryptMessage(data['Body']));
+    //data['Body'] = decryptMessage(data['Body']);
+    res.send(data).status(200);
+  })
+  .catch((err) => {
+    console.log("Error in /download path! Error: ", err);
+    res.sendStatus(500);
+  })
+})
 
 
 app.listen(PORT, () => {
