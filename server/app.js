b153c7a1a38a6bb81f70703170a9812bd6ac2f54
NateLeeP <nlpruitt@gmail.com>
2021-01-04 13:13:11 -0500

    Moved uploadFile server routes to 'routes' folder

--- server/app.js
+++ server/app.js
@@ -5,10 +5,6 @@ const cors = require('cors');
 const { router } = require('./routes/route');
 const app = express();
 const PORT = 3000;
-const axios = require('axios');
-const multer = require('multer');
-const upload = multer();
-const {uploadFile, getFile, decryptMessage} = require('../util/upload-file.js');
 
 
 // Serve static assets from 'dist' folder
@@ -16,38 +12,6 @@ app.use('(/apartment)?', express.static(path.join(__dirname, '../dist')));
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(cors());
-
-app.post('/upload', upload.any(), (req, res) => {
-  // How to pass files to endpoint?
-  console.log(req.files);
-  var promises = [];
-  req.files.forEach((file) => {
-    promises.push(uploadFile(file));
-  })
-  Promise.all(promises)
-  .then((result) => {
-    //console.log(result);
-    res.sendStatus(200);
-  })
-  .catch((err) => {
-    console.log("Error in post route! Errror: ", err);
-    res.sendStatus(500);
-  })
-})
-
-app.get('/download', (req, res) => {
-  getFile("Encrypted PDF File.pdf")
-  .then((data) => {
-    console.log('Encrypted message return: ', data['Body'])
-    //console.log('Decrypt Message Return: ', decryptMessage(data['Body']));
-    //data['Body'] = decryptMessage(data['Body']);
-    res.send(data).status(200);
-  })
-  .catch((err) => {
-    console.log("Error in /download path! Error: ", err);
-    res.sendStatus(500);
-  })
-})
 
 
 
