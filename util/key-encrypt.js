f7cc7089a757a0a1532419038ac4b4cb5b4830f6
NateLeeP <37353308+NateLeeP@users.noreply.github.com>
2021-01-03 22:38:24 -0500

    Remove secret key from public repository

--- util/key-encrypt.js
+++ util/key-encrypt.js
@@ -1,6 +1,6 @@
 const { KmsKeyringNode, encrypt, decrypt } = require("@aws-crypto/client-node");
 
-const masterKeyId = "arn:aws:kms:us-east-2:865109414926:key/89e7a42e-ad2e-473c-8e5b-de2cc605dbb1";
+const masterKeyId = "";
 const keyring = new KmsKeyringNode({ masterKeyId });
 
 let plainText = "My passwords for senstive data";
