03b5a01e6d66d3e86b9effdc98150d04f72415b5
NateLeeP <nlpruitt@gmail.com>
2021-01-04 13:17:58 -0500

    Changed import paths for FileUpload components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App.jsx'
 import css from '../dist/styles/styles.css'
-import FileUpload from './components/FileUpload.jsx';
-import FileUploadOverlay from './components/FileUploadOverlay.jsx'
+import FileUpload from './components/FileUpload/FileUpload.jsx';
+import FileUploadOverlay from './components/FileUpload/FileUploadOverlay.jsx'
 
 ReactDOM.render(<FileUpload />, document.getElementById('app'));
