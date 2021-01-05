4192ac2605664026977ec6aa011ca0cba0f3340c
NateLeeP <nlpruitt@gmail.com>
2021-01-04 23:32:46 -0500

    deleted unnecessary helper functions

--- util/key-encrypt.js
+++
@@ -1,7 +0,0 @@
-const { KmsKeyringNode, encrypt, decrypt } = require("@aws-crypto/client-node");
-
-const masterKeyId = "";
-const keyring = new KmsKeyringNode({ masterKeyId });
-
-let plainText = "My passwords for senstive data";
-const { result } = await encrypt(keyring, plainText, { encryptionContext: context });
