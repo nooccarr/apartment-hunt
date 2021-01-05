360c304efd2ac85ac3d2f507c3ecebfd18ab9845
NateLeeP <nlpruitt@gmail.com>
2021-01-05 15:27:31 -0500

    Change component rendered

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ import css from '../dist/styles/styles.css'
 import FileUpload from './components/FileUpload/FileUpload.jsx';
 import FileUploadOverlay from './components/FileUpload/FileUploadOverlay.jsx'
 import VideoUpload from './components/FileUpload/VideoUpload.jsx'
+import AppliedApartments from './components/Agent/AppliedApartments.jsx';
 
-ReactDOM.render(<VideoUpload />, document.getElementById('app'));
+ReactDOM.render(<AppliedApartments agentName={'Noah Sondheim'}/>, document.getElementById('app'));
+
+//
