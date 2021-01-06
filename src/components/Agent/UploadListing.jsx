c2f7275c146cd723a2cc04e0749066a32f4b9e1d
urbanseabear <calvin.rudy@gmail.com>
2021-01-06 09:48:07 -0500

    added some styling to upload listing page

--- src/components/Agent/UploadListing.jsx
+++ src/components/Agent/UploadListing.jsx
@@ -3,7 +3,7 @@ import PlacesAutoComplete, { geocodeByAddress, getLatLng } from 'react-places-au
 import TopBanner from '../SearchResults/TopBanner';
 import VideoUpload from '../FileUpload/VideoUpload.jsx'
 import axios from 'axios';
-import DoneIcon from '@material-ui/icons/Done';
+import './uploadlisting.scss'
 
 const UploadListing = ({ searchValue, setSearchValue }) => {
   const [agent, setAgent] = useState('');
