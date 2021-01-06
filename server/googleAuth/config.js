4e7dc99abb8211fd5f20798a67f1a358a8e87928
Philip <philip.rosenberg@comcast.netgit config --global core.editor vim>
2021-01-06 16:39:37 -0500

    Added Dylans folder

---
+++ server/googleAuth/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    google: {
+      clientID: '799055902121-8fooai74o0p54tch3trjeqveo7cg70fi.apps.googleusercontent.com',
+      clientSecret: 'ZkO-DY-B0Y2a7xhhlFoVi-lm'
+    }
+  }
