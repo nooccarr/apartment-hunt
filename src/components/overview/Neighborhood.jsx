66ea944676fa90375565c77bf7e813dec054862f
Philip <philip.rosenberg@comcast.netgit config --global core.editor vim>
2021-01-06 15:02:24 -0500

    All modals now come from the selected apartmnet

--- src/components/overview/Neighborhood.jsx
+++ src/components/overview/Neighborhood.jsx
@@ -15,12 +15,20 @@ class Neighborhood extends React.Component {
         return (
             <div className='neighborhood'>
                 {this.props.location.neighborhoods.map((neighborhood, index) => {
+                    var trueName = neighborhood.split(' ')
+                    var final = ''
+                    for (var i =0; i < trueName.length; i++) {
+                        final = final + trueName[i].charAt(0).toUpperCase() + trueName[i].slice(1) + ' '
+                    }
+                    if (neighborhoods[`${neighborhood.toLowerCase()}`] === undefined) {
+                        return null
+                    } else {
                     return (
                         <div key={index}>
-                            <h3 className='neighborhoodName'>{neighborhood}</h3>
-                            <p className='neighborhoodDes'>{neighborhoods[`${neighborhood}`]}</p>
+                            <h3 className='neighborhoodName'>{final}</h3>
+                            <p className='neighborhoodDes'>{neighborhoods[`${neighborhood.toLowerCase()}`]}</p>
                         </div>
-                    )
+                    )}
                 })}
             </div>
         )}
