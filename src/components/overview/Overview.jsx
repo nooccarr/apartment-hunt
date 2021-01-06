f0747b0037994cf7f0439ff79127ad229b898247
Noah Heath <noahadamheath@gmail.com>
2021-01-06 10:20:16 -0500

    Displays videos from the database

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -2,23 +2,27 @@ import React from 'react';
 import Description from './Description.jsx';
 import Album from './Album.jsx';
 import Navigation from './navigation.jsx';
+import axios from 'axios';
 
 class Overview extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
+        this.state = {details: null};
+    }
 
-        }
+    componentDidMount () {
+        axios(`/apt${window.location.search}`).then(({data}) => {
+            this.setState({details: data});
+            console.log(data);
+        }).catch(() => console.log('No data'));
     }
 
 
-
     render () {
-
         return (
         <div>
             <Navigation/>
-            <Album/>
+            {this.state.details === null ? null : <Album details={this.state.details}/>}
             <Description/>
         </div>
         )
