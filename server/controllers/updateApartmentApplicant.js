fd22364f152929d61b5d53f8865698637808e0bb
Nate Pruitt <nlpruitt@gmail.com>
2021-01-07 00:52:47 -0500

    Added 'strict' mode to account for model lacking Object ID

--- server/controllers/updateApartmentApplicant.js
+++ server/controllers/updateApartmentApplicant.js
@@ -2,7 +2,7 @@ const Apartment = require('../../database/Apartments.js');
 
 const updateApartmentApplicant = (apartment_id, username) => {
   return Apartment.findByIdAndUpdate(apartment_id,
-    {$addToSet: { applicants: username } }).exec()
+    {$addToSet: { applicants: username } }, {strict: false}).exec()
 };
 
 exports.updateApartmentApplicant = updateApartmentApplicant;
