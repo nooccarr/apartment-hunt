c7571244d609c8086fe566cd560214e875f06d71
Matthew Schneiderman <matthewdschneiderman@gmail.com>
2021-01-08 11:14:01 -0500

    Fixed web socket

--- src/components/chatbox/backend/chatbox_server.js
+++ src/components/chatbox/backend/chatbox_server.js
@@ -1,37 +1,37 @@
-const express = require('express');
-const app = express();
-const port = 5000;
-var server = require('http').createServer(app);
+// const express = require('express');
+// const app = express();
+// const port = 5000;
+// var server = require('http').createServer(app);
 
-const io = require('socket.io')(server, {
-  cors: {
-    origin: 'http://localhost:3000',
-    credentials: true,
-    allowedHeaders: ['chatRooms'],
-  },
-});
+// const io = require('socket.io')(server, {
+//   cors: {
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//     allowedHeaders: ['chatRooms'],
+//   },
+// });
 
-io.on('connection', (socket) => {
-  socket.on('disconnect', (reason) => {
-    console.log('');
-  });
+// io.on('connection', (socket) => {
+//   socket.on('disconnect', (reason) => {
+//     console.log('');
+//   });
 
-  socket.on('join room', (data) => {
-    console.log('a user connected');
-    socket.join(data.room);
-  });
+//   socket.on('join room', (data) => {
+//     console.log('a user connected');
+//     socket.join(data.room);
+//   });
 
-  socket.on('leave room', (data) => {
-    console.log('a user disconnected');
-    socket.leave(data.room);
-  });
+//   socket.on('leave room', (data) => {
+//     console.log('a user disconnected');
+//     socket.leave(data.room);
+//   });
 
-  socket.on('new message', (data) => {
-    socket.broadcast
-      .to(data.room)
-      .emit('receive message', [data.messageObj, data.room]);
-  });
-});
+//   socket.on('new message', (data) => {
+//     socket.broadcast
+//       .to(data.room)
+//       .emit('receive message', [data.messageObj, data.room]);
+//   });
+// });
 
-server.listen(port);
-console.log('running on', port);
+// server.listen(port);
+// console.log('running on', port);
