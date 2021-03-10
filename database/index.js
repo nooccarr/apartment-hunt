7754dbb638c89c564d720d94e0555239533ba399
Soohyun Lim <d2axasap@gmail.com>
2021-03-09 21:11:22 -0500

    Added favicon

--- database/index.js
+++ database/index.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
-const db = mongoose.connect('mongodb://localhost/apartmentHunt?authSource=admin', {promiseLibrary: global.Promise});
+// const db = mongoose.connect('mongodb://localhost/apartmentHunt?authSource=admin', {promiseLibrary: global.Promise});
 
-// require('dotenv').config();
+require('dotenv').config();
 
-// // NEW DATABASE SERVER IP: 3.14.136.176
-// // const { user, password } = require('./dbconfig.js');
-// const db = mongoose.connect(
-//   // `mongodb://${user}:${password}@3.14.136.176:27017/apartmentHunt?authSource=admin`,
-//   `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@3.14.136.176:27017/apartmentHunt?authSource=admin`,
-//   { promiseLibrary: global.Promise }
-// );
+// NEW DATABASE SERVER IP: 3.14.136.176
+// const { user, password } = require('./dbconfig.js');
+const db = mongoose.connect(
+  // `mongodb://${user}:${password}@3.14.136.176:27017/apartmentHunt?authSource=admin`,
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@3.14.136.176:27017/apartmentHunt?authSource=admin`,
+  { promiseLibrary: global.Promise }
+);
 
 module.exports = db;
