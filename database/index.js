adc58dfa06dd5fe6330808a87b64695ecac2d450
Soohyun Lim <d2axasap@gmail.com>
2021-03-10 11:40:10 -0500

    mongoose is now connected to local database

--- database/index.js
+++ database/index.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
-// const db = mongoose.connect('mongodb://localhost/apartmentHunt?authSource=admin', { promiseLibrary: global.Promise, useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connect('mongodb://localhost/apartmentHunt?authSource=admin', { promiseLibrary: global.Promise, useNewUrlParser: true, useUnifiedTopology: true });
 
-require('dotenv').config();
+// require('dotenv').config();
 
-// NEW DATABASE SERVER IP: 3.14.136.176
-// const { user, password } = require('./dbconfig.js');
-const db = mongoose.connect(
-  // `mongodb://${user}:${password}@3.14.136.176:27017/apartmentHunt?authSource=admin`,
-  `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@3.14.136.176:27017/apartmentHunt?authSource=admin`,
-  { promiseLibrary: global.Promise, useNewUrlParser: true, useUnifiedTopology: true }
-);
+// // NEW DATABASE SERVER IP: 3.14.136.176
+// // const { user, password } = require('./dbconfig.js');
+// const db = mongoose.connect(
+//   // `mongodb://${user}:${password}@3.14.136.176:27017/apartmentHunt?authSource=admin`,
+//   `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@3.14.136.176:27017/apartmentHunt?authSource=admin`,
+//   { promiseLibrary: global.Promise, useNewUrlParser: true, useUnifiedTopology: true }
+// );
 
 module.exports = db;
