7cb089dd1053a49272b21f55065fbaa331acf591
Soohyun Lim <d2axasap@gmail.com>
2021-03-10 23:49:28 -0500

    Changed url for db connection

--- database/index.js
+++ database/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const db = mongoose.connect('mongodb://localhost/apartmentHunt?authSource=admin', { promiseLibrary: global.Promise, useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connect('mongodb://localhost:27017/ApartmentHunt', { promiseLibrary: global.Promise, useNewUrlParser: true, useUnifiedTopology: true });
+// const db = mongoose.connect('mongodb://localhost:27017/apartmentHunt', { promiseLibrary: global.Promise, useNewUrlParser: true, useUnifiedTopology: true });
 
 // require('dotenv').config();
 
